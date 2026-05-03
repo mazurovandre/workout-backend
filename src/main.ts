9e425431c81ed288c5dbadf8aa49a333df12b0e9
Andrey Mazurov <mazurovandre@gmail.com>
2026-05-03 21:20:54 +0300

    Fixed prisma, deleted mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    expressBasicAuth({
-      users: {
-        [process.env.BASIC_AUTH_USER ?? 'admin']:
-          process.env.BASIC_AUTH_PASSWORD ?? 'admin',
-      },
-      challenge: true,
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 
